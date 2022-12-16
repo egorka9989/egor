c17f9207dac2a92616099dc4416366674b5e4ecd
egorka9989 <120741384+egorka9989@users.noreply.github.com>
2022-12-16 20:13:19 +0300

    Create Структуры и классы.cpp

---
+++ Структуры и классы.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+using namespace std;
+
+class Call
+{
+private:
+    int first;
+    double second;
+public:
+    Call() {};
+    Call(int min, double tariff)
+    {
+        this->first = min;
+        this->second = tariff;
+    }
+    ~Call() {}
+    double cost()
+    {
+        double p = first * second;
+        return p;
+    }
+};
+
+
+int main()
+{
+    int time;
+    double price;
+    cout << "Time (min):\t";
+    cin >> time;
+    cout << "Price per minute:\t";
+    cin >> price;
+
+    Call c(time, price);
+    cout << "The payment is: " << c.cost();
+
+    system("pause");
+    return 0;
+}
