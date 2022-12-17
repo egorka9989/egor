de974d86f0974c38bd0508b53ef8442c07b900df
egorka9989 <120741384+egorka9989@users.noreply.github.com>
2022-12-17 14:35:00 +0300

    Векторы

---
+++ Векторы.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream.h>
+#include <vcl.h>
+#include <conio.h>
+#include <stdlib.h>
+using namespace std;
+int main()
+{
+	int n, i;
+	float max = 0;
+	cout << "Enter integer value: ";
+	cin >> n;
+	float* p_darr = new float[n];
+	for (int i = 0; i < n; i++) {
+		randomize;
+		p_darr[i] = (float)(rand()) / RAND_MAX;
+		cout << "Value of " << i << " element is " << p_darr[i] << endl;
+	}
+	for (i = 0; i < n; i++) {
+		if (p_darr[i] > max)
+			max = p_darr[i];
+	}
+	cout << "Maks element = " << max << endl;
+	float b = p_darr[0];
+	p_darr[0] = max;
+	max = b;
+	for (int i = 0; i < n; i++) {
+		cout << "Value of " << i << " element is " << p_darr[i] << endl;
+	}
+	delete[] p_darr;
+	system("pause");
+	return 0;
+}
