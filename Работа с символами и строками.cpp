f2531dad3a82011230630fe5dadb1240bc878b3f
egorka9989 <120741384+egorka9989@users.noreply.github.com>
2022-12-17 16:40:23 +0300

    в

---
+++ Работа с символами и строками.cpp
@@ -0,0 +1,16 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int lat = 0, rus = 0;
+    string s;
+    cin >> s;
+    for (int i = 0; i < s.size(); ++i) {
+        if (s[i] >= 'a' && s[i] <= 'z')
+            ++lat;
+        if (s[i] >= 'а' && s[i] <= 'я')
+            ++rus;
+    }
+    cout << lat << " " << rus << endl;
+    return 0;
+}
---
+++ Стандартные функции работы со строками.cpp
@@ -0,0 +1,13 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    getline(cin, s);
+    int pos = s.find(" ");
+    int pos1 = s.rfind(" ");
+    if (pos1 + 1 && pos != pos1) {
+        cout << s.substr(pos + 1, pos1 - pos);
+    }
+    return 0;
+}
