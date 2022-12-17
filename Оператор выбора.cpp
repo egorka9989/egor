2d32b1a5853b3228245b79c2b335c71020cebf36
egorka9989 <120741384+egorka9989@users.noreply.github.com>
2022-12-17 16:25:21 +0300

    Create Оператор выбора.cpp

---
+++ Оператор выбора.cpp
@@ -0,0 +1,65 @@
+﻿#include <stdio.h>
+
+void m(const char* a, int mas);
+
+int main(void)
+{
+    int mast = 0;  /* масть */
+    int dost = 0;  /* достоинство */
+
+    printf("Введите достоинство (от 6 до 14) и масть карты (от 1 до 4): ");
+    scanf("%d%d", &dost, &mast);
+
+    switch (mast)
+    {
+    case 1:
+        m("бубен", dost);
+        break;
+    case 2:
+        m("червей", dost);
+        break;
+    case 3:
+        m("треф", dost);
+        break;
+    case 4:
+        m("пик", dost);
+        break;
+    default: printf("не найдено\n");
+    }
+    return 0;
+}
+
+void m(const char* a, int mas)
+{
+    switch (mas)
+    {
+    case 6:
+        printf("шестерка %s", a);
+        break;
+    case 7:
+        printf("семерка %s", a);
+        break;
+    case 8:
+        printf("восьмерка %s", a);
+        break;
+    case 9:
+        printf("девятка %s", a);
+        break;
+    case 10:
+        printf("десятка %s", a);
+        break;
+    case 11:
+        printf("валет %s", a);
+        break;
+    case 12:
+        printf("дама %s", a);
+        break;
+    case 13:
+        printf("король %s", a);
+        break;
+    case 14:
+        printf("туз %s", a);
+        break;
+    default:  printf("не найдено\n");
+    }
+}
