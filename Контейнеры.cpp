6c7ebb8a8958713ca965e6e181acf402c3d2f91a
egorka9989 <120741384+egorka9989@users.noreply.github.com>
2022-12-25 17:47:19 +0300

    Create Контейнеры.cpp

---
+++ Контейнеры.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+
+int main()
+{
+	std::vector<int> vec = { 1,2,3,4,5 };
+	std::swap(*std::max_element(vec.begin(), vec.end()), *std::min_element(vec.begin(), vec.end()));
+	std::copy(vec.begin(), vec.end(), std::ostream_iterator<int>(std::cout, ","));
+	std::cout << std::endl;
+	return 0;
+}
